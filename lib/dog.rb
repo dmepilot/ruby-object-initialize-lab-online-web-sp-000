d1d02c3723cf0e4e42cdf263504831fedc25daf3
Dean Erickson <dmepilot@yahoo.com>
2020-06-17 12:44:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,6 @@ class Dog
   def initialize(name, breed)
     @name=name
     @breed = breed
+    @breed = "Mutt"
   end
-
-  def breed= (breed)
-    @breed= breed
-  end
-    
-    def breed
-      @breed 
-    end
 end
